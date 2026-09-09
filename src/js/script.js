79e7e47d29a5071f980ec4ca4df87bc63ad6b3fe
Mauricio <mauriciomarangoni0601@gmail.com>
2026-09-09 12:03:13 -0300

    feat:funcionalidade do SlideShow

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,32 @@
+//ARRAY DE IMAGENS
+let imagens=["/src/assets/carro1.jpeg",
+             "/src/assets/carro2.jpeg",
+             "/src/assets/carro3.jpeg"];
+
+//POSIÇÃO QUE VAI INICIAR AS IMAGENS
+let index=0;
+//TEMPO PARA TROCAR AS IMAGENS
+let tempo=3000; //3 SEGUNDOS
+
+//FUNÇÃO DO SLIDESHOW
+function SlideShow(){
+    //DOM PEGA O ID E PASSA O CAMINHO DAS IMAGENS
+    document.getElementById("imgBanner").src=imagens[index];
+    //INCREMENTO DAS IMAGENS
+    index++;
+    //ESTRUTURA CONDICIONAL IF
+    if(index == imagens.length){
+        //INICIA NA POSIÇÃO 0(OU SEJA SEMPRE VOLTA PARA O 0)
+        index=0;
+    }
+    //MÉTODO SETIME PARA EXECUTAR A FUNÇÃO(CALLBACK ) E
+    //CHAMAR O TEMPO
+    setTimeout("SlideShow()",tempo)
+}
+SlideShow();
+
+
+
 const menuIcone = document.getElementById("menu-icone");
 const navMenu = document.getElementById("nav-menu");
 
